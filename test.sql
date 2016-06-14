21d61dfc597a767f1dddf939dff5d38c6b75d448
emtiazzahid <emtiazzahid@gmail.com>
2016-06-14 17:15:46 +0600

    sql file added

---
+++ test.sql
@@ -0,0 +1,49 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 14, 2016 at 01:13 PM
+-- Server version: 10.1.10-MariaDB
+-- PHP Version: 7.0.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `test`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `oop`
+--
+-- in use(#1932 - Table 'test.oop' doesn't exist in engine)
+-- Error reading data: (#1932 - Table 'test.oop' doesn't exist in engine)
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_test`
+--
+-- in use(#1932 - Table 'test.tbl_test' doesn't exist in engine)
+-- Error reading data: (#1932 - Table 'test.tbl_test' doesn't exist in engine)
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_user`
+--
+-- in use(#1932 - Table 'test.tbl_user' doesn't exist in engine)
+-- Error reading data: (#1932 - Table 'test.tbl_user' doesn't exist in engine)
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
